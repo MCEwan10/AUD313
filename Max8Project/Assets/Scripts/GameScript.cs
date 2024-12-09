2054f5914d6749563b72972698a45823e9b4a79f
Harry Findlay <harryfindlay01@gmail.com>
2024-12-09 01:57:22 +0000

    Gameplay loop updated
    Still not fully functional but progress

--- Max8Project/Assets/Scripts/GameScript.cs
+++ Max8Project/Assets/Scripts/GameScript.cs
@@ -18,8 +18,8 @@ public class GameScript : MonoBehaviour
     public GameObject textSayingScore;
     public int attackType;
     public int attackPitch;
-    public int playerAttakType;
-    public int playerAttakPitch;
+    public int playerAttackType;
+    public int playerAttackPitch;
     /*referencing tags in unity*/
     public GameObject hitObj;
     public GameObject blockObj;
@@ -52,7 +52,7 @@ public class GameScript : MonoBehaviour
     private bool buttonPressedDuringTimer = false;
 
     //flag for round checking
-    private bool isRoundOver = false;
+    private bool _bhasAttacked = false;
 
     // Start is called before the first frame update
     void Start()
@@ -62,10 +62,10 @@ public class GameScript : MonoBehaviour
         attackType = 0;
         attackPitch = 0;
         startTime = 10.0f;/*initial timer duration*/
-        isGameStarted=false;
+        isGameStarted=true;
         currentTime = startTime;
-        hitObj.SetActive(false);
-        blockObj.SetActive(false);
+        //hitObj.SetActive(true);
+        //blockObj.SetActive(true);
         textSayingScore.SetActive(false);
         pitches.Add(-0.2f);
         pitches.Add(1f);
@@ -96,43 +96,35 @@ public class GameScript : MonoBehaviour
 
         BGM.volume = .5f;
     }
-    public bool ResetTimer(bool wasHealthReduced)
-    {
-        if (startTime !< 0.4f && wasHealthReduced == false) currentTime = startTime;
-        timerStarted = true;
-        buttonPressedDuringTimer = false;
 
-        return buttonPressedDuringTimer;
-    }
-    public void GenerateNumbers(bool isTimerRunning)
+    public void SetAttackType(bool isTimerRunning)
     {
         if (!isTimerRunning) //Maybe the loop error has to do with the run statment being a bool and not a proper check
         {
-        /*pick attackType (Sword or Punch)*/
-        attackType = Random.Range(1, 3);
-        /*pick attackPitch (High, Neutral or Low)*/
-        attackPitch = Random.Range(0, 3);
-        Debug.Log("$Attack :"+attackType + " Pitch :"+attackPitch);
-        /*set the sound the the correct attack*/
-        if (attackType == 1)
-        {
-            soundOnUI.clip = punchAttack;
-            clipLength = punchAttack.length;
-        }
-        else
-        {
-            soundOnUI.clip = swordAttack;
-            clipLength = swordAttack.length;
-        }
+            /*pick attackType (Sword or Punch)*/
+            attackType = Random.Range(1, 3);
+            /*pick attackPitch (High, Neutral or Low)*/
+            attackPitch = Random.Range(0, 3);
+            Debug.Log("$Attack :"+attackType + " Pitch :"+attackPitch);
+            /*set the sound the the correct attack*/
+            if (attackType == 1)
+            {
+                soundOnUI.clip = punchAttack;
+                clipLength = punchAttack.length;
+            }
+            else
+            {
+                soundOnUI.clip = swordAttack;
+                clipLength = swordAttack.length;
+            }
 
-        soundOnUI.pitch = pitches[attackPitch];
+            soundOnUI.pitch = pitches[attackPitch];
+            _bhasAttacked = true;
         }
     }
     public void RunAttackCycle()
     {
-        hitObj.SetActive(false);
-        blockObj.SetActive(false);
-        GenerateNumbers(timerStarted);
+        //GenerateNumbers(timerStarted);
         /*send to Max*/
         /*return sounds*/
         /*play sound*/
@@ -153,7 +145,7 @@ public class GameScript : MonoBehaviour
                 Debug.Log("Time's up to press button!");
 
                 /*check input and if match attack and pitch*/
-                if (playerAttakType==attackType && playerAttakPitch == attackPitch)
+                if (playerAttackType==attackType && playerAttackPitch == attackPitch)
                 {
                     //player wins round
                     PlayerBlocked();
@@ -169,13 +161,13 @@ public class GameScript : MonoBehaviour
                 ResetTimer(currentTime);
                 buttonPressedDuringTimer = false;
 
-                isRoundOver = true;
+                _bhasAttacked = true;
 
                 //reset assets to init states
                 ResetGameLoop();
 
                 //start new round
-                if(health > 0 && !isRoundOver)
+                if(health > 0 && !_bhasAttacked)
                 {
                     StartCoroutine(Delay(delayAmount));
                 }
@@ -187,10 +179,16 @@ public class GameScript : MonoBehaviour
 
     private void ResetGameLoop()
     {
+        //reset UI
         hitObj.SetActive(false);
         blockObj.SetActive(false);
-        //GenerateNumbers(timerStarted);
-        isRoundOver = false;
+
+        //flag variable
+        _bhasAttacked = false;
+        buttonPressedDuringTimer = false;
+
+        //reset timer
+        ResetTimer(currentTime);
     }
 
     public void ButtonPressed()
@@ -202,6 +200,7 @@ public class GameScript : MonoBehaviour
     {
         /*show score*/
         textSayingScore.SetActive(true);
+        isGameStarted = false;
     }
 
     public void PlayerHit()
@@ -217,30 +216,46 @@ public class GameScript : MonoBehaviour
             soundOnHit.clip = swordHit;
             clipLength = swordHit.length + 0.5f;
         }
+        
+        //set UI and audio
         hitObj.SetActive(true);
         soundOnHit.Play();
+
+        //set sup-loop to force a slowdown of gameplay loop
         while (clipLength>0.0f)
         {
           clipLength -= Time.deltaTime;  
         }
+
+        StartCoroutine(Delay((int)clipLength));
+
+        //load sound
+        //grab length and add a dramatic pause
+        //play sound
+        //remove life
         soundOnUI.clip = healthLost;
-        clipLength = healthLost.length + 0.7f; //longer pause for dramatic effect
+        clipLength = healthLost.length + 1f; //longer pause for dramatic effect
         soundOnUI.Play();
         health--;
-        while (clipLength>0.0f)
+
+        //sub-loop 
+        while (clipLength>=0.0f)
         {
           clipLength -= Time.deltaTime;  
         }
-        if (health > 0)
+
+        //lose conditional
+        if (health >= 0)
         {
-            isRoundOver = true;
+            _bhasAttacked = true;
             ResetTimer(currentTime);
         }
-        else
+        else //you suck git gud
         {
             GameOver();
         }
 
+        //delay
         StartCoroutine(Delay(delayAmount));
     }
 
@@ -257,18 +272,26 @@ public class GameScript : MonoBehaviour
             soundOnBlock.clip = swordBlocked;
             clipLength = swordBlocked.length + 0.5f;
         }
+
+        //set UI and audio
         blockObj.SetActive(true);
         soundOnBlock.Play();
+
+        //force into sub-loop to force a stop in the code loop
         while (clipLength>0.0f)
         {
           clipLength -= Time.deltaTime;  
         }
+
+        //general pdates
         score += 100;
         startTime -= 0.2f;
 
+        //delay
         StartCoroutine(Delay(delayAmount));
 
-        isRoundOver = true;
+        //end and reset
+        _bhasAttacked = true;
         ResetTimer(currentTime);
     }
     // Update is called once per frame
@@ -282,17 +305,50 @@ public class GameScript : MonoBehaviour
         }
 
         RegisterInput();
-        if(isGameStarted)
-        {
-        if (health > 0 && !isRoundOver)
-        {
-            StartCoroutine(Delay(delayAmount));
 
-            RunAttackCycle();
-        }
-        }
-        
+        //gameplay loop
+        if(isGameStarted) //gameloop check
+        {
+            if (health > 0 && !_bhasAttacked) //valid round
+            {
+                StartCoroutine(Delay(delayAmount)); //delay between rounds
+
+                SetAttackType(timerStarted); //generate attack type and audio
+                soundOnUI.Play(); //play 
+
+                //force gameplay loop into a sub-loop while audio is active
+                while (clipLength > 0.0f)
+                {
+                    clipLength -= Time.deltaTime;
+                }
+            }
+
+            currentTime -= Time.deltaTime; //start countdown
+
+            if (currentTime <= 0.0f && !buttonPressedDuringTimer) //if valid timer and null input
+            {
+                soundOnUI.pitch = 1f;//reset pitch for attack and heart
+                Debug.Log("Time's up to press button!");
+
+
+                //check player input for matches
+                if (playerAttackType == attackType && playerAttackPitch == attackPitch)
+                {
+                    //player wins round
+                    PlayerBlocked();
+                }
+                else
+                {
+                    //lose round
+                    PlayerHit();
+                }
+
+                if(health >= 0)
+                    ResetGameLoop();
+            }
     }
+        
+}
 
     public int delayAmount = 0;
     IEnumerator Delay(int delayAmount)
@@ -311,28 +367,28 @@ public class GameScript : MonoBehaviour
         /*get player input*/
         if (Input.GetKeyDown(KeyCode.Keypad4))
         {
-            playerAttakType = 1;
-            Debug.Log(playerAttakType);
+            playerAttackType = 1;
+            Debug.Log(playerAttackType);
         }
         if (Input.GetKeyDown(KeyCode.Keypad6))
         {
-            playerAttakType = 2;
-            Debug.Log(playerAttakType);
+            playerAttackType = 2;
+            Debug.Log(playerAttackType);
         }
         if (Input.GetKeyDown(KeyCode.Keypad8))
         {
-            playerAttakPitch = 2;
-            Debug.Log(playerAttakPitch);
+            playerAttackPitch = 2;
+            Debug.Log(playerAttackPitch);
         }
         if (Input.GetKeyDown(KeyCode.Keypad5))
         {
-            playerAttakPitch = 1;
-            Debug.Log(playerAttakPitch);
+            playerAttackPitch = 1;
+            Debug.Log(playerAttackPitch);
         }
         if (Input.GetKeyDown(KeyCode.Keypad2))
         {
-            playerAttakPitch = 0;
-            Debug.Log(playerAttakPitch);
+            playerAttackPitch = 0;
+            Debug.Log(playerAttackPitch);
         }
         if(Input.GetKeyDown(KeyCode.Keypad0))
         {
